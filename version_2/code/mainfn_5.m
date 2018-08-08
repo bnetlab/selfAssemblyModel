e704331088784a3b781e93d95338d084fd73ef0e
PratipRana <ranap@vcu.edu>
2018-08-08 12:59:19 -0400

    bin size updated to 0.02

--- version_2/code/mainfn_5.m
+++ version_2/code/mainfn_5.m
@@ -1,5 +1,6 @@
 % main function
 % longer observation and input time range
+% bin size 0.02
 
 clear all
 clc
@@ -10,7 +11,7 @@ T=1;
 
 % mu=1.0;
 % lambda=1.0;
-bin=0.05;
+bin=0.02;
 Pmin=0;
 Pmax=20;
 tmin=-10;
@@ -24,11 +25,11 @@ Z_point=(Zmax-Zmin)/bin+1;
 
 
 % parameters
-[t2,t3]=meshgrid([tmin:0.05:tmax]);
+[t2,t3]=meshgrid([tmin:bin:tmax]);
 t2=reshape(t2,[],1);
 t3=reshape(t3,[],1);
 
-[tau_2,tau_3]=meshgrid([Pmin:0.05:Pmax]);
+[tau_2,tau_3]=meshgrid([Pmin:bin:Pmax]);
 tau_2=reshape(tau_2,[],1);
 tau_3=reshape(tau_3,[],1);
 
