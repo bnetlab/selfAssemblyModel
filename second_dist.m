3703b9cf1a08f11751c4b24b81dcab8841e34589
PratipRana <ranap@vcu.edu>
2019-04-20 20:22:35 -0400

    final BICT model

--- second_dist.m
+++
@@ -1,8 +0,0 @@
-% calculate equation 2
-
-
-function Q = second_dist(mu, lambda, t2, t3, tau2, tau3 )
-for i =1:length(t2)
-    Q(i)=integral(@(x)pdf('InverseGaussian',x,mu,lambda).*pdf('InverseGaussian',x+t2(i)-tau2,mu,lambda).*pdf('InverseGaussian',x+t3(i)-tau3,mu,lambda),0,50);
-end
-end
