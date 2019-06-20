2c2c88a06198f31bf69c15a7f9bbfe2aede55c3a
PratipRana <ranap@vcu.edu>
2019-06-19 23:18:09 -0400

    N molecules

--- code/Simulation/sim_model.py
+++ code/Simulation/sim_model.py
@@ -9,17 +9,12 @@ Created on Wed Jun 12 13:02:14 2019
 V = l/mu
 D = 2 * sigma^2 = (2*l^2) / lambda 
 
-Things to to:
-    1. Check with inverse gaussian distribution
-    2. decrease bin_size
-    3. compare with 3 molecules 
-    4. genearlize for higher moleculaes
-
 """
 
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+plt.switch_backend('agg')
 from scipy.stats import invgauss
 import math
 import time
@@ -29,7 +24,7 @@ def brownian(d,v,sigma,t_bin=0.001):
     # simulate brownian motion
     
     pos= 0
-    t= 0
+    t= 1
     v_t = v*t_bin
     while pos < d:
         pos = pos + v_t + (sigma * np.random.randn() * math.sqrt(t_bin))
@@ -47,79 +42,102 @@ def eq1s4(t2,t3,t4,T):
     arr = np.sort(arr, axis=None)
     a= bool(np.heaviside (np.absolute(arr[2] -arr[1])- T, 0)) and bool((np.heaviside(np.absolute(arr[3] -arr[2])- T, 0)))
     return a
+
+def eq1(t,T):
+    s = 2
+    arr =np.sort(t, axis=None)
+    
+    t_bind=1
+    t_next=2
+    while t_next < len(arr):
+        if(arr[t_next] -arr[t_bind] > T):
+            s+=1; t_bind = t_next; t_next+=1
+        else:
+            t_next+=1
+    s_arr = np.zeros((len(arr)-1,), dtype=int)
+    s_arr[s-2] = 1
+    return s_arr
         
-def sim(d,v,sigma,T,tau, no_sim=100000):
+def sim(N,d,v,sigma,T,tau, no_sim=100000):
     
-    # simulate 4 particle model
+    # simulate N particle model
     
-    s_count = np.zeros(3)
+    s_count = np.zeros(N-1)
     for i in range(1,no_sim):
         
-        tau1 = np.random.exponential(tau);
-        tau2 = tau1 + np.random.exponential(tau);
-        tau3 = tau2 + np.random.exponential(tau);
+        tauT = np.zeros((N,),dtype=float)
+        tauT[0] = 0;
+        for i in range(1,len(tauT)):
+            tauT[i] =tauT[i-1]+ np.random.exponential(tau);
+            
+        t = np.zeros((N,),dtype=float)
+        for i in range(0,len(t)):
+            t[i] =tauT[i]+ brownian(d,v,sigma);
+            
+        s = eq1(t,T)
         
-        t1 = tau1 + brownian(d,v,sigma)
-        t2 = tau2 + brownian(d,v,sigma)
-        t3 = tau3 + brownian(d,v,sigma)
-        
-        s4 = eq1s4(t1,t2,t3,T)
-        s2 = eq1s2(t1,t2,t3,T)
-        
-        if s2 == True:
-            s_count[0] += 1
-        elif s4 == True:
-            s_count[2] += 1
-        else:
-            s_count[1] +=1
+        s_count +=s
             
     s_prob = s_count/no_sim
     return(s_prob)
 
-def isIG(d,v,sigma,T,tau, no_sim=100000, n_bins=20):
+def isIG(d,v,sigma, no_sim=100000, n_bins=20):
     
     # check the distribution is IG or not
     
     simData = np.empty([no_sim,])
     for i in range(1,no_sim):
         simData[i] = brownian(d,v,sigma)
-    simData = simData[(simData <= 5*(d/v))]
-    plt.hist(simData, normed=True, bins=n_bins)
+    
+    fig = plt.figure()
+    
+    plt.hist(simData, normed=True, bins=n_bins, range = (0,5*(d/v)))
     plt.ylabel('Prob')
     
-    igData = invgauss.rvs(1, size=no_sim)
-    igData = igData[(igData <= 5*(d/v))]
+    mu=d/v; lamda=d**2/sigma**2;
+    igData =  invgauss.rvs(mu/lamda, scale=lamda, size=no_sim)
+
+    plt.hist(igData, normed=True, histtype='step',range = (0,5*(d/v)), bins=n_bins, color='r')
     
-    plt.hist(igData, normed=True, histtype='step', bins=n_bins, color='r')
+    
+    filename = 'plot' + str(d) + str(v) + str(sigma) + '.png'
+    plt.savefig(filename)
     
 def main():
     
     # run for different parameter combination
     
-    d=1; v=1; sigma=1;
-    count=0; Data = np.zeros((64,8),dtype=float)
+    N=4 ;d=1; v=1; sigma=1;
+    t_range = [0.5,1,1.5,2,2.5,3,3.5,4]
+    tau_range = [0.5,1,1.5,2,2.5,3,3.5,4]
+    count=0; Data = np.zeros((len(t_range)*len(tau_range),5+N-1),dtype=float)
     start = time.time()
-    for T in [0.5,1,1.5,2,2.5,3,3.5,4]:
-        for tau in [0.5,1,1.5,2,2.5,3,3.5,4]:
-            res = sim(d,v,sigma,T,tau)
+    for T in t_range:
+        for tau in tau_range:
+            res = sim(N,d,v,sigma,T,tau)
             Data[count,0] = d; Data[count,1] = v; Data[count,2] = sigma;
             Data[count,3] = T; Data[count,4] = tau;
-            Data[count,5] = res[0]; Data[count,6] = res[1]; Data[count,7] = res[2];
+            for i in range(len(res)):
+                Data[count,5+i] = res[i]
             count+=1;
             print("Done :", count, " Time : ",time.time() - start)
-    pd.DataFrame(Data).to_csv('dataSimulation.csv')
+    
+    pd.DataFrame(Data).to_csv('dataSimulation'+str(N)+'.csv')
     
 def test():
     
     # test mode
-    
-    d=1; v=1; sigma=1; T=1; tau=1; 
+#    for d in [1,2,4]:
+#        for v in [1,2,4]:
+#            for sigma in [1,0.5,0.25]:
+#                isIG(d,v,sigma)
+                
+    N=4; d=1; v=1; sigma=1; T=2; tau=2;
     start = time.time()
-    res = sim(d,v,sigma,T,tau)
+    res = sim(N,d,v,sigma,T,tau)
     print("result : ", res, " Time: ", time.time()-start)
-    isIG(d,v,sigma,T,tau)
         
-test()    
+main()    
     
     
     
