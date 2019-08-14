cbb62db299a9c16c4dd4c514f9f2a5253065301d
PratipRana <ranap@vcu.edu>
2019-08-14 01:04:45 -0400

    start

--- code/AB_self_assembly/Ab_assembly.py
+++ code/AB_self_assembly/Ab_assembly.py
@@ -8,3 +8,19 @@ Created on Thu Aug  1 13:46:07 2019
 
 import numpy as np
 
+f_alpha=True
+
+del_t=0.001
+v=1
+D=0.5
+l=10
+m=1
+del_G = 1
+
+
+del_x = 2*D*del_t*np.random.randn()
+v_eff = v + del_x/del_t 
+del_p = m*del_x/del_t
+
+
+
